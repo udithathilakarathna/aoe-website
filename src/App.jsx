28e5cf432656070a4b3bf5a73ea98af317ead420
Nipun Dhanushka <164483654+dhanushka9987@users.noreply.github.com>
2026-05-22 01:25:57 +0530

    Completed research page (#3)
    Co-authored-by: dhanushka9987 <ndanushka16889@email.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 import Home from "./pages/Home";
+import Research from "./pages/Research.jsx";
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/research"
+          element={<Research />}
+        />
+
+      </Routes>
+
+    </HashRouter>
+  );
 }
 
 export default App;
