51abe88ebd366dfa2e02270c9b85c18ffd0e0458
Dinindu Arachchi <dinu21897@gmail.com>
2026-05-23 15:18:44 +0530

    Add Renewable Energy and Robotics club pages (#5)
    * Add Renewable Energy and Robotics club pages
    
    * complete Renewable Energy Society club page
    
    * complete robotic club page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import {
 
 import Home from "./pages/Home";
 import Research from "./pages/Research.jsx";
+import RE_Club from "./pages/RE_Club.jsx";
+import Robotics_Club from "./pages/Robotics_Club.jsx";
 
 function App() {
   return (
@@ -21,6 +23,16 @@ function App() {
         <Route
           path="/research"
           element={<Research />}
+        />
+
+        <Route
+          path="/re_club"
+          element={<RE_Club />}
+        />
+
+        <Route
+          path="/robotics_club"
+          element={<Robotics_Club />}
         />
 
       </Routes>
